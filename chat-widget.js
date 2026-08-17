30ec3bc5be23fbe45b6a102debab244978a19cc6
Henri Dejonghe <henridejonghe@MacBook-Air-4.local>
2026-08-17 12:35:26 +0200

    Update chatbot widget

---
+++ chat-widget.js
@@ -0,0 +1,21 @@
+(function(d, t) {
+  var v = d.createElement(t);
+  var s = d.getElementsByTagName(t)[0];
+
+  v.onload = function() {
+    window.voiceflow.chat.load({
+      verify: {
+        projectID: '6a5606df2901b669ad990604'
+      },
+      url: 'https://general-runtime.voiceflow.com',
+      voice: {
+        url: 'https://runtime-api.voiceflow.com'
+      }
+    });
+  };
+
+  v.src = 'https://cdn.voiceflow.com/widget-next/bundle.mjs';
+  v.type = 'text/javascript';
+
+  s.parentNode.insertBefore(v, s);
+})(document, 'script');
